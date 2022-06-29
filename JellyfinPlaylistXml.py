9e14712d3767372c0f52ddd8db510d1ee583ae40
LambdaGaming <28662514+LambdaGaming@users.noreply.github.com>
2022-06-29 11:27:34 -0400

    Added better support for duplicate files

--- JellyfinPlaylistXml.py
+++ JellyfinPlaylistXml.py
@@ -1,5 +1,6 @@
 import humanfriendly
 import os
+from pathlib import Path
 import shutil
 import xml.etree.ElementTree as ET
 
@@ -19,26 +20,29 @@ for file in FileList:
     size += os.path.getsize( file )
 
 print( f"Total size of playlist is {humanfriendly.format_size( size )}." )
-proceed = input( "Do you want to continue? (y/n)" )
+input( "Press any key to begin the copying process..." )
 progress = 0
-blacklist = """?"'"""
-if proceed == "y":
-    dest = input( "Enter the destination path for the songs: " )
-    print( "Copying songs..." )
-    for file in FileList:
-        if os.path.exists( os.path.join( dest, os.path.basename( file ) ) ):
-            print( f"{os.path.basename( file )} was already copied. Skipping..." )
-            continue
-        for char in blacklist:
-            file = file.replace( char, "" )
-        try:
-            shutil.copy( file, dest )
-            perc = ( progress / len( FileList ) ) * 100
-            if perc % 10 == 0:
-                print( f"{perc}%" )
-        except:
-            print( f"Something went wrong while copying {file}. Skipping..." )
-        progress += 1
-    print( f"Successfully copied {len( FileList )} songs" )
+blacklist = """?"'%{&}$!`+|=:;@*<>#""" # Having these characters in the file name can cause issues on certain platforms
+dest = input( "Enter the destination path for the songs: " )
+print( "Copying songs..." )
+for file in FileList:
+    count = 2
+    namepath = Path( file )
+    while os.path.exists( os.path.join( dest, os.path.basename( file ) ) ):
+        file = os.path.join( os.path.dirname( file ), f"{namepath.stem}({count}){namepath.suffix}" ) # Add number to end of file name if a file with same name already exists
+        count += 1
+        print( f"{os.path.basename( path )} already exists. Adding number to end of name..." )
+        
+    for char in blacklist:
+        file = file.replace( char, "" ) # Remove blacklisted characters from media name
+    try:
+        shutil.copy( file, dest )
+        perc = ( progress / len( FileList ) ) * 100
+        if perc % 10 == 0:
+            print( f"{perc}%" ) # Display progress percentage in multiples of 10
+    except:
+        print( f"Something went wrong while copying {file}. Skipping..." )
+    progress += 1
+print( f"Successfully copied {len( FileList )} songs" )
 print( "Process finished." )
 input( "Press any key to continue..." )
